1244e74b10de56850a5b391cacee66e889387ff2
Utkarsh Pathrabe <uapathrabe@gmail.com>
2022-01-08 00:11:14 +0530

    Create fetchWeather.js

---
+++ src/api/fetchWeather.js
@@ -0,0 +1,14 @@
+import axios from 'axios';
+
+const URL = 'https://api.openweathermap.org/data/2.5/weather';
+
+export const fetchWeather = async (query) => {
+    const { data } = await axios.get(URL, {
+        params: {
+            q: query,
+            units: 'metric',
+            APPID: process.env.REACT_APP_FETCH_WEATHER_API_KEY,
+        }
+    });
+    return data;
+};
